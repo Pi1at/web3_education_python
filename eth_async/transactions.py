83c83dabdc2b439c7441416390aca2f5921dcc33
Anatoly Chumakov <grand.anatol@ya.ru>
2024-01-21 22:31:11 +0300

    change gas_price from static method to client method

--- eth_async/transactions.py
+++ eth_async/transactions.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
-from web3 import Web3
-
 from .models import TokenAmount
 
 if TYPE_CHECKING:
@@ -13,6 +11,5 @@ class Transactions:
     def __init__(self, client: Client) -> None:
         self.client = client
 
-    @staticmethod
-    async def gas_price(w3: Web3) -> TokenAmount:
-        return TokenAmount(amount=await w3.eth.gas_price(), wei=True)
+    async def gas_price(self) -> TokenAmount:
+        return TokenAmount(amount=await self.client.w3.eth.gas_price, wei=True)
